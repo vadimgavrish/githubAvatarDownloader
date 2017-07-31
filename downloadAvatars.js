27bb500be0bc293a7ce3aab34931b6211af00d3a
Vadim Gavrish <vadim@gavrish.ca>
2017-07-31 22:10:53 +0000

    final downloadAvatars exercise

--- downloadAvatars.js
+++ downloadAvatars.js
@@ -1,49 +1,39 @@
 var request = require('request');
+var fs = require('fs');
 
 console.log('Welcome to the Gihub Avatar Downloader!')
 
 var GITHUB_USER = 'vadimgavrish';
 var GITHUB_TOKEN = '6dc806145c584b233aa636944eba7e47365da69e';    
 
-
 function getRepoContributors(repoOwner, repoName, cb) {
 
-    var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '' + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
-    var options = {
-        url: requestURL,
-        headers: {'User-Agent': 'GitHub Avatar Downloader - Student Project'}
-    };
+    if (!process.argv[3]) {
+        console.log("Invalid input!")
+    } else {
+        var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '' + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
+        var options = {
+            url: requestURL,
+            headers: {'User-Agent': 'GitHub Avatar Downloader - Student Project'}
+        };
 
-    request(options, function (error, response, body) {
-
-        var data = JSON.parse(body);
-
-
-        cb(error, data);
-
-
-    });
-    
-    console.log(requestURL);
-
+        request(options, function (error, response, body) {
+            var data = JSON.parse(body);
+            cb(error, data);
+        });
+        console.log(requestURL);
+    }
 }
 
-
-getRepoContributors("jquery", "jquery", function(error, data) {
-  
-
-
-
+getRepoContributors(process.argv[2], process.argv[3], function(error, data) {
     for (var i = 0; i < data.length; i++) {
-
-        console.log(data[i].avatar_url);
-
+       downloadImageByUrl(data[i].avatar_url, "./avatars/" + data[i].login + ".jpg");
     }
-
-
-
-
-
 });
+
+function downloadImageByUrl (url, filePath) {
+    request.get(url)
+        .pipe(fs.createWriteStream(filePath));
+}
